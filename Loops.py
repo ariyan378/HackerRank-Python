0647f0f40ed2c324f035323fccdc29f70e4bc31e
Ariyan Islam hridoy <161193575+ariyan378@users.noreply.github.com>
2026-01-17 02:02:46 +0600

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,5 @@
+def power_func(num):
+    for i in range(num):
+        print(i**2)
+
+power_func(int(input("Enter Your Number :")))
---
+++ Print_Function.py
@@ -0,0 +1,8 @@
+n = int(input())
+output = ''
+for i in range(n+1):
+    output+=str(i)
+
+
+print(output)    
+
