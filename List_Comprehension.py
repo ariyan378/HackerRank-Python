9fdac4114b54f6f7e83065b8b7fbcb0f9791a736
Ariyan Islam hridoy <161193575+ariyan378@users.noreply.github.com>
2026-01-18 01:06:42 +0600

    Add files via upload

---
+++ List_Comprehension.py
@@ -0,0 +1,14 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+'''for i in range (x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i+j+k !=n:
+                list1 = (i,j,k)
+                print(list(list1),end=',')'''
+
+list1 = [(i,j,k) for i in range(x+1)for j in range(y+1)for k in range (z+1) if i+j+k!=n]
+print(list1)
